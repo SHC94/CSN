ef49b4f18bf9abe5946c4d07f703ea49b72c23ca
SHC94 <gudcjf4757@naver.com>
2022-07-04 18:45:14 +0900

    Initial commit

---
+++ src/main/java/com/csn/csn/CsnApplication.java
@@ -0,0 +1,13 @@
+package com.csn.csn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csn/csn/CsnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csn.csn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
