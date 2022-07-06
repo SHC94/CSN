02edc00960b41071ff8fc2d77bb57ff6ac02848b
SHC94 <gudcjf4757@naver.com>
2022-07-06 20:10:40 +0900

    네이버 로그인 API 프로필 조회 API

--- src/main/java/com/csn/csn/main/controller/MainController.java
+++ src/main/java/com/csn/csn/main/controller/MainController.java
@@ -2,13 +2,21 @@ package com.csn.csn.main.controller;
 
 import com.csn.csn.main.entity.Tab;
 import com.csn.csn.main.service.MainService;
+import com.github.scribejava.core.model.OAuth2AccessToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -24,13 +32,14 @@ public class MainController {
      * @return
      */
     @RequestMapping("/")
-    public String mainHome(Model model) {
+    public String mainHome(Model model, HttpSession session) {
         log.info("mainHome start ===================");
         
         //탭 메뉴 조회
         List<Tab> tabList = mainService.selectTab(); 
         model.addAttribute("tabList", tabList);
-        
+
         return "home";
     }//end mainHome()
+
 }//end class()
