80bb7a0abedd083d3c8e85767e57752cef497798
lalalalz <lalalalz@naver.com>
2022-07-07 14:10:37 +0900

    수정/최진수/applicatino.yml 프로필설정

--- src/main/java/com/csn/csn/Member/Member.java
+++ src/main/java/com/csn/csn/Member/Member.java
@@ -1,11 +1,14 @@
 package com.csn.csn.Member;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
 public class Member {
 
     @Id @GeneratedValue
@@ -20,4 +23,6 @@ public class Member {
     private String birthday;
     private String email;
     private String phone;
+
+
 }
