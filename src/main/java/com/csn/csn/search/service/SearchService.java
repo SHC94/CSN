b576f12aeae1b4a8da74f8c847e6c04d67b068e3
lalalalz <lalalalz@naver.com>
2022-07-12 17:18:05 +0900

    서치 서비스 인터페이스 구축

--- src/main/java/com/csn/csn/search/service/SearchService.java
+++ src/main/java/com/csn/csn/search/service/SearchService.java
@@ -1,40 +1,16 @@
 package com.csn.csn.search.service;
 
 import com.csn.csn.member.entity.Member;
-import com.csn.csn.member.repository.MemberRepository;
 import com.csn.csn.search.entity.Search;
-import com.csn.csn.search.repository.SearchRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class SearchService {
+public interface SearchService {
 
-    private final SearchRepository searchRepository;
-    private final MemberRepository memberRepository;
+    void doSearch(String loginId, String query);
 
-    public void doSearch(String loginId, String query) {
-        memberRepository.findByLoginId(loginId)
-                .ifPresent((m) -> {
-                    searchRepository.save(new Search(m, query));
-                });
-    }
+    List<Search> getRecentSearch(Member member);
 
-    public List<Search> getRecentSearch(Member member) {
-        return searchRepository.findByMember(member, 10);
-    }
-
-    public List<Search> getSearchAfterSpecificBuildTime(LocalDateTime buildTime, Integer limit) {
-        if (buildTime == null) {
-            throw new IllegalArgumentException("검색 조회 기준 시간이 잘못되었습니다.");
-        }
-
-        return searchRepository.findByBuildTime(buildTime, limit);
-    }
+    public List<Search> getSearchAfterSpecificBuildTime(LocalDateTime buildTime, Integer limit);
 }
